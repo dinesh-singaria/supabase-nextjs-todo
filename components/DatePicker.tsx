59123713c3f24776dac12c4376765606a7691e06
Dinesh Singaria <dineshsingaria.111@gmail.com>
2025-01-15 19:03:31 +0000

    Date-picker updated

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,77 +1,86 @@
-import React from "react";
-import {
-  Input,
-  Popover,
-  PopoverHandler,
-  PopoverContent,
-} from "@material-tailwind/react";
-import { format } from "date-fns";
-import { DayPicker } from "react-day-picker";
-import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.min.css";
 
-const DatePicker = () => {
-  const [date, setDate] = React.useState<Date>();
+const Datepick = () => {
+  const [startDate, setStartDate] = useState(
+    new Date().setDate(new Date().getDate() + 3)
+  );
   return (
-    <div className="pr-2">
-      <Popover placement="bottom">
-        <PopoverHandler>
-          <Input
-            label="Due Date"
-            onChange={() => null}
-            value={date ? format(date, "PPP") : ""}
-          />
-        </PopoverHandler>
-        <PopoverContent>
-          <DayPicker
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            showOutsideDays
-            className="border-0"
-            classNames={{
-              caption: "flex justify-center py-2 mb-4  items-center",
-              caption_label: "text-sm font-medium text-gray-900",
-              nav: "flex items-center",
-              nav_button:
-                "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
-              nav_button_previous: "absolute left-1.5",
-              nav_button_next: "absolute right-1.5",
-              table: "w-full border-collapse",
-              head_row: "flex font-medium text-gray-900",
-              head_cell: "m-0.5 w-9 font-normal text-sm",
-              row: "flex w-full mt-2",
-              cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-              day: "h-9 w-9 p-0 font-normal",
-              day_range_end: "day-range-end",
-              day_selected:
-                "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
-              day_today: "rounded-md bg-gray-200 text-gray-900",
-              day_outside:
-                "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
-              day_disabled: "text-gray-500 opacity-50",
-              day_hidden: "invisible",
-            }}
-            components={{
-              IconLeft: ({ ...props }) => (
-                <ChevronLeftIcon {...props} className="h-4 w-4 stroke-2" />
-              ),
-              IconRight: ({ ...props }) => (
-                <ChevronRightIcon {...props} className="h-4 w-4 stroke-2" />
-              ),
-            }}
-            // components={{
-            //   IconLeft: ({ ...props }) => (
-            //     <ChevronLeftIcon {...props} className="h-4 w-4 stroke-2" />
-            //   ),
-            //   IconRight: ({ ...props }) => (
-            //     <ChevronRightIcon {...props} className="h-4 w-4 stroke-2" />
-            //   ),
-            // }}
-          />
-        </PopoverContent>
-      </Popover>
+    <div className="relative pr-2 px-1">
+      <DatePicker
+        wrapperClassName="input-attribute"
+        className="bg-white appearance-none px-4 py-4 rounded-lg shadow border-black text-black"
+        showIcon
+        withPortal
+        excludeDateIntervals={[
+          {
+            start: new Date(),
+            end: new Date(new Date().setDate(new Date().getDate())),
+          },
+        ]}
+        selected={new Date(startDate)}
+        onChange={(date) => {
+          if (date !== null) {
+            setStartDate(date.getTime());
+          }
+        }}
+        monthsShown={1}
+        renderCustomHeader={({
+          monthDate,
+          customHeaderCount,
+          decreaseMonth,
+          increaseMonth,
+        }) => (
+          <div>
+            <button
+              aria-label="Previous Month"
+              className={
+                "react-datepicker__navigation react-datepicker__navigation--previous"
+              }
+              style={
+                customHeaderCount === 1 ? { visibility: "hidden" } : undefined
+              }
+              onClick={decreaseMonth}
+            >
+              <span
+                className={
+                  "react-datepicker__navigation-icon react-datepicker__navigation-icon--previous"
+                }
+              >
+                {"<"}
+              </span>
+            </button>
+            <span className="react-datepicker__current-month">
+              {monthDate.toLocaleString("en-US", {
+                month: "long",
+                year: "numeric",
+              })}
+            </span>
+            <button
+              aria-label="Next Month"
+              className={
+                "react-datepicker__navigation react-datepicker__navigation--next"
+              }
+              style={
+                customHeaderCount === 1 ? { visibility: "hidden" } : undefined
+              }
+              onClick={increaseMonth}
+            >
+              <span
+                className={
+                  "react-datepicker__navigation-icon react-datepicker__navigation-icon--next"
+                }
+              >
+                {">"}
+              </span>
+            </button>
+          </div>
+        )}
+      />
+      <i className="fas fa-calendar absolute top-1/4 right-4 text-black"></i>
     </div>
   );
 };
 
-export default DatePicker;
+export default Datepick;
